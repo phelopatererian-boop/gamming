de54d42d372f1fcce8010f7190b1ac525ea0275e
phelopatererian-boop <phelopatererian@gmail.com>
2026-04-29 20:54:55 +0300

    Add files via upload

---
+++ login/signin.js
@@ -0,0 +1,34 @@
+const Button = document.querySelector('.submit');
+const Name = document.querySelector('.Name');
+const tel = document.querySelector('.tel');
+const Email = document.querySelector('.Email');
+const Password = document.querySelector('.password');
+
+Button.addEventListener("click", function () {
+
+  if (Name.value === "" || Email.value === ""|| tel.value === "" || Password.value === "") {
+    window.alert("Write your information please");
+      return;
+  }
+  if (!Email.value.includes('@')) {
+    window.alert("Invalid email address. Please include an @ character.");
+  return;
+  }
+  if (tel.value.length < 11) {
+  window.alert("Enter a valid phone number");
+  return;
+}
+  if (Password.value.length < 6) {
+  window.alert("Password must be at least 6 characters");
+  return;
+}
+else{
+ 
+  // لو كل حاجة تمام
+  window.alert("تم التسجيل بنجاح ");
+  window.location.href = "index.html";
+}
+});
+function togglePassword() {
+  Password.type = Password.type === "password" ? "text" : "password";
+}
